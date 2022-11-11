ca14e8a280601519ef8aff27f45f26e18d7bf0cd
yswan <wyuansheng@126.com>
2022-11-11 22:32:57 +0800

    backtrace/README.md

---
+++ backtrace/README.md
@@ -0,0 +1,3 @@
+# backtrace print
+- 捕捉进程异常退出的堆栈信息
+- 堆栈信息日志保存路径:`${HOME}/log/${ProcessName}stacktrace.log`
--- backtrace/examples/printtrace.c
+++ backtrace/examples/printtrace.c
@@ -14,10 +14,14 @@ void *thr_fn(void *arg)
 
 int main()
 {
+    char *env_path = (char *)getenv("HOME");
+    printf("\nstack trace print in [%s/log/]\n\n", env_path);
+
     moniter_signal();
     pthread_t tid;
     pthread_create(&tid, NULL, thr_fn, NULL);
 
     pthread_join(tid,NULL);
+
     return 0;
 }
